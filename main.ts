4fee1433ec813e84b8269ec58ce9c57aba0997e7
Cyfine <58298819+Cyfine@users.noreply.github.com>
2023-07-14 20:17:30 +0800

    modify(main.ts)

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, TFile } from 'obsidian';
 
 // Remember to rename these classes and interfaces!
 
@@ -10,86 +10,65 @@ const DEFAULT_SETTINGS: MyPluginSettings = {
 	mySetting: 'default'
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
 
-	async onload() {
-		await this.loadSettings();
+function addTagsEmojiToTitle(file:TFile){
+	let note = this.app.workspace.getActiveFile();
+	// Check if the changed file is the current note
+	if (file.path === note.path) {
+		// Get the tags of the current note
+		let tags = this.app.metadataCache.getFileCache(note).tags;
+		// Get the current note title
+		let noteTitle = note.basename;
+		// Check if there are any tags
+		if (tags) {
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+			// Loop through the tags
+			let emojis: string[] = []
+			for (let tag of tags) {
+				// Get the tag name
+				let tagName = tag.tag;
+				// Check if the tag name contains an emoji
+				if (/(?!#)(\p{Emoji})/gu.test(tagName)) {
+					// Get the emoji from the tag name	
+					let tagEmojis = Array.from(tagName.match(/(?!#)(\p{Emoji})/gu) ?? []);
+					tagEmojis = tagEmojis.filter(element => !emojis.includes(element))
+					
+					emojis = [...emojis, ...tagEmojis]
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
 				}
 			}
-		});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+			let noteTitleWithoutEmoji = noteTitle.replace(/^(\p{Emoji}(\p{Variation_Selector})?)+\s*/gu, '');
+			
+			if(emojis.length > 0){ 
+				 let emojiHeader = emojis?.join('') ?? ''
+				 let newNoteTitle =  emojiHeader + ' ' + noteTitleWithoutEmoji; 
+				 this.app.fileManager.renameFile(note, newNoteTitle + '.md');
+			}
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
+		} else {
+			// If there are no tags, remove all emojis from the note title
+			// Replace all emojis with an empty string
+			let newNoteTitle = noteTitle.replace(/\p{Emoji}/gu, '');
+			// Trim any extra spaces
+			newNoteTitle = newNoteTitle.trim();
+			// Rename the note file with the new title
+			this.app.fileManager.renameFile(note, newNoteTitle + '.md');
+		}
 	}
 }
+
+
+export default class EmojiPlugin extends Plugin {
+	// Define a method to run when your plugin is loaded
+	async onload() {
+		// Register a hook to listen for changes in the metadata of notes
+		this.registerEvent(
+			this.app.metadataCache.on('changed', addTagsEmojiToTitle.bind(this))
+		);
+	}
+}
+
 
 class SampleModal extends Modal {
 	constructor(app: App) {
@@ -97,12 +76,12 @@ class SampleModal extends Modal {
 	}
 
 	onOpen() {
-		const {contentEl} = this;
+		const { contentEl } = this;
 		contentEl.setText('Woah!');
 	}
 
 	onClose() {
-		const {contentEl} = this;
+		const { contentEl } = this;
 		contentEl.empty();
 	}
 }
@@ -116,11 +95,11 @@ class SampleSettingTab extends PluginSettingTab {
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+		containerEl.createEl('h2', { text: 'Settings for my awesome plugin.' });
 
 		new Setting(containerEl)
 			.setName('Setting #1')
